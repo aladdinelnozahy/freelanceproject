7724f020d2395a52e93396a75a33fe7aeea6453c
Aladdin Elnozahy <aladdin.elnozahy@gmail.com>
2020-12-30 23:49:20 +0200

    fix list freelancer -admin

--- admin/freelancers.php
+++ admin/freelancers.php
@@ -1,7 +1,6 @@
 <?php 
   $sql="SELECT f_name , f_id FROM freelancers ORDER BY f_name";
   $query= mysqli_query($connection ,$sql);
-  $rows=  mysqli_fetch_array($query);
 ?>
 <!-- start card -->
 <select class="form-control" id="free">
